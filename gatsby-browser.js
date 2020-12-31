a5c01a45239f04abfe55d956f648413f97d0dd62
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-12-31 23:57:57 +0530

    font-package change

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,4 @@
-import 'fontsource-montserrat';
-import 'fontsource-bungee';
+import '@fontsource/montserrat';
+import '@fontsource/montserrat/700.css';
+import '@fontsource/montserrat/300.css';
+import '@fontsource/bungee';
