819020ebeacfaf37fe07c19acd95b9d233046a4b
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-12-31 23:59:25 +0530

    made the blog cards responsive

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Card = styled.div`
-  width: 318px;
-  height: 360px;
-  box-shadow: 7px 3px 4px rgba(0, 0, 0, 0.29);
+const StyledLink = styled.a`
+  text-decoration: none;
+  color: black;
 `;
 
-const BlogCard = ({ title, imageUrl }) => {
+const Card = styled.div`
+  height: 360px;
+  box-shadow: 7px 3px 4px rgba(0, 0, 0, 0.29);
+
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+`;
+
+const BlogCard = ({ title, imageUrl, postUrl }) => {
   return (
-    <div>
+    <StyledLink href={postUrl} target="_blank" rel="noopener noreferrer">
       <Card>
         <img src={imageUrl} alt={title} />
       </Card>
       <h2>{title}</h2>
-    </div>
+    </StyledLink>
   );
 };
 
--- src/templates/member.js
+++ src/templates/member.js
@@ -3,13 +3,37 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import styled from 'styled-components';
 import BlogCard from '../components/BlogCard';
-import Parser from 'rss-parser';
+import { getCoverImageUrlFromMediumPost } from '../utils/helpers';
+import useMediumFeed from '../hooks/useMediumFeed';
 // import MemberImage from '../components/MemberImage';
 
-const MemberContainer = styled.div``;
+const MemberContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-areas:
+    'bio'
+    'picture'
+    'blogs';
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-areas:
+      'picture bio'
+      'blogs blogs';
+  }
+
+  @media (min-width: 1024px) {
+    grid-gap: 32px;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-areas:
+      'picture bio bio'
+      'picture blogs blogs';
+  }
+`;
 
 const Bio = styled.div`
   padding: 0 32px;
+  box-sizing: border-box;
   h2 {
     text-transform: uppercase;
     font-weight: 700;
@@ -25,35 +49,56 @@ const Bio = styled.div`
 
   p {
     font-size: 1.6em;
-    margin: 3px 0;
+    margin: 12px 0;
     line-height: 28px;
+  }
+
+  @media (min-width: 768px) {
+    p {
+      margin: 44px 0;
+    }
   }
 `;
 
 const PictureContainer = styled.div`
+  grid-area: picture;
   padding: 0 32px;
   height: 600px;
   box-sizing: border-box;
   width: 100%;
   max-width: 540px;
-  background-color: ${(props) => (props.background ? props.background : '#1DCCCC')};
+
+  div:last-of-type {
+    background-color: #1dcccc;
+  }
 
   p {
     font-size: 1.6em;
     line-height: 28px;
     font-weight: 300;
     font-style: italic;
+    position: relative;
+
+    span {
+      font-size: 200px;
+      text-align: left;
+      color: rgb(0, 0, 0, 0.26);
+      display: inline-block;
+      position: absolute;
+      top: 84px;
+    }
   }
 `;
 
 const BlogContainer = styled.div`
+  grid-area: blogs;
   padding: 0 20px;
-  display: flex;
-  flex-wrap: wrap;
   box-sizing: border-box;
+  max-width: 1020px;
   width: 100%;
 
   > h2 {
+    display: inline-block;
     text-transform: uppercase;
     padding-top: 20px;
     font-weight: bold;
@@ -61,21 +106,21 @@ const BlogContainer = styled.div`
   }
 `;
 
-const parser = new Parser();
-const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+const BlogPostGrid = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  justify-content: center;
+
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 32px;
+  }
+`;
 
 const MemberTemplate = ({ data }) => {
   const member = data.markdownRemark;
-
-  const [blogPosts, setBlogPosts] = React.useState([]);
-  React.useEffect(() => {
-    parser
-      .parseURL(CORS_PROXY + `https://medium.com/feed/@${member.frontmatter.medium}`)
-      .then((response) => setBlogPosts(response.items))
-      .catch((error) => console.log(error));
-  }, []);
-
-  console.log(blogPosts);
+  const [blogPosts] = useMediumFeed(member.frontmatter.medium);
 
   return (
     <Layout>
@@ -86,14 +131,30 @@ const MemberTemplate = ({ data }) => {
           <p>{member.frontmatter.description}</p>
         </Bio>
         <PictureContainer>
-          <div>image goes here</div>
-          <p>{member.frontmatter.quote}</p>
+          <div>Social media icons</div>
+          <div>
+            <img src="" alt="profile" />
+            <p>
+              <span>“</span>
+              {member.frontmatter.quote}
+              <span>“</span>
+            </p>
+          </div>
         </PictureContainer>
         <BlogContainer>
           <h2>Blogs</h2>
-          {blogPosts.map((post) => {
-            return <BlogCard key={post.guid} title={post.title} />;
-          })}
+          <BlogPostGrid>
+            {blogPosts.map((post) => {
+              return (
+                <BlogCard
+                  key={post.guid}
+                  title={post.title}
+                  imageUrl={getCoverImageUrlFromMediumPost(post['content:encoded'])}
+                  postUrl={post.link}
+                />
+              );
+            })}
+          </BlogPostGrid>
         </BlogContainer>
       </MemberContainer>
     </Layout>
