006a94251c277a62918abbc05e52127e0532a7f3
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2021-01-12 16:52:18 +0530

    adjusted css to stretch the slider items

--- src/styles/SliderHomePage.js
+++ src/styles/SliderHomePage.js
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 
 const SliderHomePage = styled.div`
-  background:${(props) => (props.dark == true ? 'black':'#eaf0f8')};
+  background:${(props) => (props.dark == true ? 'black' : '#eaf0f8')};
   width: 281px;
   height: 125px;
   padding-left:21px;
   padding-top: 10px;
   padding-right: 24px;
+  flex: 1;
+  margin-top: 40px;
  
   .heading-scroll-section {
     text-align: left;
@@ -15,14 +17,14 @@ const SliderHomePage = styled.div`
     font-family: 'Helvetica', 'Neue';
     letter-spacing: 0px;
     
-    color:${(props) => (props.dark == true ? 'white':"#707070")};
+    color:${(props) => (props.dark == true ? 'white' : '#707070')};
     opacity: 1;
     display: flex;
   }
   .circle-scroll-section {
     height: 24px;
     width: 23px;
-    background-color:${(props) => (props.dark == true ? 'white':"black")} ;
+    background-color:${(props) => (props.dark == true ? 'white' : 'black')} ;
     border-radius: 50%;
   
     display: flex;
@@ -37,7 +39,7 @@ const SliderHomePage = styled.div`
     width: 32px;
     height: 0px;
     border: 5px solid;
-    border-color:${(props) => (props.dark == true ? '#DDF521':"#707070")} ;
+    border-color:${(props) => (props.dark == true ? '#DDF521' : '#707070')} ;
     opacity: 1;
     margin-left:0px;
    
@@ -50,7 +52,7 @@ const SliderHomePage = styled.div`
     font-family: 'Helvetica', 'Neue';
     letter-spacing: 0px;
     
-    color: ${(props) => (props.dark == true ? 'white':"#585050")} ;
+    color: ${(props) => (props.dark == true ? 'white' : '#585050')} ;
     opacity: 1;
     margin-top: 10px;
     width: 225px;
@@ -59,6 +61,13 @@ const SliderHomePage = styled.div`
       display: flex;
       align-items:center;
     
+  }
+
+  @media (min-width: 1024px) {
+    height: 240px;
+    .heading-scroll-section {
+      font-size: 23px;
+    }
   }
 }
 `;
