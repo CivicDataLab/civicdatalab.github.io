9844ce9f30587394cd8f1930e284dd3578f60429
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2021-01-20 00:42:27 +0530

    moved h1 out of p

--- src/components/OurPillars.js
+++ src/components/OurPillars.js
@@ -1,33 +1,30 @@
 import React from 'react';
 import HeroText from '../styles/HeroText';
-import OurPillarsStyle from "../styles/OurPillars";
-const OurPillars= (props) => {
-    return(
-        <OurPillarsStyle>
-        <div className={'pillars-container'}>
-            <div className={"image-container"}>
-                <p><HeroText>Data</HeroText></p>
-                <div className={"image-placeholder"}></div>
-
-            </div>
-            <div className={"image-container"}>
-                <p><HeroText>Tech</HeroText></p>
-                <div className={"image-placeholder"}></div>
-
-            </div>
-            <div className={"image-container"}>
-                <p><HeroText>Design</HeroText></p>
-                <div className={"image-placeholder"}></div>
-
-            </div>
-            <div className={"image-container"}>
-                <p><HeroText>Social Science</HeroText></p>
-                <div className={"image-placeholder"}></div>
-
-            </div>
+import OurPillarsStyle from '../styles/OurPillars';
+const OurPillars = (props) => {
+  return (
+    <OurPillarsStyle>
+      <div className={'pillars-container'}>
+        <div className={'image-container'}>
+          <HeroText>Data</HeroText>
+          <div className={'image-placeholder'}></div>
         </div>
-        </OurPillarsStyle>
-    )
-}
+        <div className={'image-container'}>
+          <HeroText>Tech</HeroText>
+          <div className={'image-placeholder'}></div>
+        </div>
+        <div className={'image-container'}>
+          <HeroText>Design</HeroText>
+
+          <div className={'image-placeholder'}></div>
+        </div>
+        <div className={'image-container'}>
+          <HeroText>Social Science</HeroText>
+          <div className={'image-placeholder'}></div>
+        </div>
+      </div>
+    </OurPillarsStyle>
+  );
+};
 
 export default OurPillars;
