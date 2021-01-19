cf9f459c6e2384b84acbcc6661e1403172afa73e
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2021-01-20 00:42:15 +0530

    added images and mouse events to show/hide info

--- src/components/MemberImageBox.js
+++ src/components/MemberImageBox.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
+import Image from 'gatsby-image';
 
 const ImageBox = styled(Link)`
-  display: inline-block;
-  width: 138px;
-  height: 207px;
+  display: flex;
+  flex-direction: column;
   background: #f2f2f2;
   position: relative;
-
-  img {
-    width: 100%;
-    height: 100%;
-  }
+  text-decoration: none;
 
   .member-details {
     position: absolute;
-    padding: 15px 0;
     bottom: 0;
+    padding: 15px 0;
     width: 100%;
     display: flex;
     flex-direction: column;
@@ -36,35 +32,28 @@ const ImageBox = styled(Link)`
     font-size: 15px;
     font-family: Montserrat;
     color: #ffffff;
+    text-align: center;
   }
 
-  @media (min-width: 768px) {
-    width: 167px;
-    height: 250px;
-  }
-
-  @media (min-width: 1280px) {
-    width: 200px;
-    height: 300px;
-  }
   @media (min-width: 1600px) {
-    width: 276px;
-    height: 414px;
-
     .member-details {
       padding: 21px 0 28px;
     }
   }
 `;
 
-const MemberImageBox = ({ link, name, role }) => {
+const MemberImageBox = ({ link, name, role, image }) => {
+  const [displayInfo, setDisplayInfo] = React.useState(false);
+
   return (
-    <ImageBox to={link}>
-      <img src="" alt="member" />
-      <span className="member-details">
-        <span className="member-name">{name.split(' ')[0]}</span>
-        <span className="member-desg">{role}</span>
-      </span>
+    <ImageBox to={link} onMouseOver={() => setDisplayInfo(true)} onMouseLeave={() => setDisplayInfo(false)}>
+      <Image fluid={image} />
+      {displayInfo ? (
+        <span className="member-details">
+          <span className="member-name">{name.split(' ')[0]}</span>
+          <span className="member-desg">{role}</span>
+        </span>
+      ) : null}
     </ImageBox>
   );
 };
