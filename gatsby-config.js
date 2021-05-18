1cd6fc370651c52ef469ac4337cae84d6bcba328
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2021-05-18 15:17:15 +0530

    added sitemap builder

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,7 @@ module.exports = {
     'gatsby-transformer-remark',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    'gatsby-plugin-react-helmet'
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap'
   ]
 };
