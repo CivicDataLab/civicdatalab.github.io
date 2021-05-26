fada3ccc58a0e1288ee68134cb9e42cc4a7f9166
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2021-05-26 19:22:46 +0530

    updated work with us section issue #32

--- src/components/WorkHomePage.js
+++ src/components/WorkHomePage.js
@@ -2,36 +2,42 @@ import React from 'react';
 import WorkHomePageStyle from '../styles/WorkHomePage';
 import { Link } from 'gatsby';
 import HeroText from '../styles/HeroText';
+import pattern from '../images/assets/pattern.png';
 
 const WorkHomePage = () => {
   return (
-    <WorkHomePageStyle>
+    <WorkHomePageStyle style={{ backgroundImage: pattern }}>
       <div className={'container-work-section'}>
         <div className={'content'}>
-          <HeroText className={'work-part'}>Work </HeroText>
-          <HeroText className={'sub-text'}> With Us!</HeroText>
+          <HeroText light className={'work-part'}>
+            Work{' '}
+          </HeroText>
+          <HeroText light className={'sub-text'}>
+            {' '}
+            With Us!
+          </HeroText>
           <hr className={'work-home-page'}></hr>
           <p className={'first-text-block'}>
             Let’s co-create the next solution enabling sustainable social change in your area of work
           </p>
         </div>
         <div className={'circle-wrapper'}>
-          <div className={'content-circle'} id="first-circle">
+          <Link to="/contact" className={'content-circle'} id="first-circle">
             <div className={'circle'}>
-              <img src={require('../images/join-icon.svg')} alt="Join us" />
+              <img src={require('../images/join-icon.png')} alt="Join us" />
             </div>
             <div className={'below-text'}>
               <p>Partner With Us</p>
             </div>
-          </div>
-          <div className={'content-circle'}>
+          </Link>
+          <Link to="/openings" className={'content-circle'}>
             <div className={'circle'}>
-              <img src={require('../images/partner-icon.svg')} alt="Partner with us" />
+              <img src={require('../images/partner-icon.png')} alt="Partner with us" />
             </div>
-            <Link to="/openings" className={'below-text'}>
+            <div className={'below-text'}>
               <p>Join Us</p>
-            </Link>
-          </div>
+            </div>
+          </Link>
         </div>
       </div>
     </WorkHomePageStyle>
