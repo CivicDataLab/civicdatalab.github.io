9069889771ce9b4e964e69ab23570d5d4155de44
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2021-05-26 22:31:45 +0530

    made our partners section to auto scroll

--- src/components/OurPartners.js
+++ src/components/OurPartners.js
@@ -5,12 +5,29 @@ import Image from 'gatsby-image';
 import ScrollContainer from 'react-indiana-drag-scroll';
 
 const OurPartners = ({ partners }) => {
+  const scrollContainer = React.useRef(null);
+
+  React.useEffect(() => {
+    if (scrollContainer.current) {
+      let scrollWidth = 0;
+      setInterval(() => {
+        if (scrollWidth > scrollContainer.current.scrollWidth + 1000) {
+          console.log('reached the end');
+          scrollWidth = 0;
+        } else {
+          scrollWidth = scrollWidth + 2;
+          scrollContainer.current.scrollTo(scrollWidth, 0);
+        }
+      }, 20);
+    }
+  }, []);
+
   return (
     <OurPartnersStyle>
       <div className={'content'}>
         <HeroText className={'section-heading'}>Our Partners</HeroText>
       </div>
-      <ScrollContainer className="partners-container" vertical={false}>
+      <ScrollContainer className="partners-container" vertical={false} innerRef={scrollContainer}>
         {partners.map((partner) => (
           <a key={partner.id} href={`https://${partner.name}`} target="_blank" rel="noreferrer noopener">
             <Image fixed={partner.childImageSharp.fixed} />
