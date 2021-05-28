08a5c49b3e0c7a3c925b5e26ede89894df386e04
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2021-05-28 13:41:06 +0530

    updated image path for meta tags

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,8 @@ module.exports = {
     summary: '',
     description:
       'A research lab working on the intersection use data, tech, design and social science to strengthen the course of civic engagements in India.',
-    siteUrl: 'https://civicdatalab.in',
+    siteUrl: 'https://civicdatalab.in/website-new',
+    image: '/cdl_logo.png',
     social: {
       twitter: '',
       github: '',
