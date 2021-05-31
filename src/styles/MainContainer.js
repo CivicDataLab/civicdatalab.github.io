edfe7407fb6ad37d93046c1be2f57a83b39c82c8
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2021-05-31 11:58:55 +0530

    fixed the padding on less than smaller resolution screens

--- src/styles/MainContainer.js
+++ src/styles/MainContainer.js
@@ -4,6 +4,20 @@ const MainContainer = styled.div`
   max-width: 1640px;
   width: 100%;
   margin: auto;
+  padding: 0 16px;
+  box-sizing: border-box;
+
+  @media (min-width: 1280px) {
+    padding: 0 52px;
+  }
+
+  @media (min-width: 1440px) {
+    padding: 0 72px;
+  }
+
+  @media (min-width: 1920px) {
+    padding: 0;
+  }
 `;
 
 export default MainContainer;
