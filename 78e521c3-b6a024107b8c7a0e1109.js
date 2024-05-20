cb264fd9f37e5d648b794501b2d03ad54f1977b6
aashimgarg <aashimgarg@users.noreply.github.com>
2024-05-20 06:54:17 +0000

    deploy: f3dd43167d2858e7456111dd0730f108a7a21f00

--- 78e521c3-b6a024107b8c7a0e1109.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{v7au:function(t,n,a){"use strict";a.d(n,"a",(function(){return i}));var c=a("Lnxd");function i(t){return Object(c.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M10 3V5H5V19H19V14H21V20C21 20.5523 20.5523 21 20 21H4C3.44772 21 3 20.5523 3 20V4C3 3.44772 3.44772 3 4 3H10ZM17.7071 7.70711L12 13.4142L10.5858 12L16.2929 6.29289L13 3H21V11L17.7071 7.70711Z"}}]})(t)}}}]);
-//# sourceMappingURL=78e521c3-b6a024107b8c7a0e1109.js.map
